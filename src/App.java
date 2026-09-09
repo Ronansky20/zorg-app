0a79144f176f56d28f76bfa5144119604847e62a
Ronansky <48455711+Ronansky20@users.noreply.github.com>
2026-09-10 00:30:44 +0200

    Enhance Person record to include age and update user details printing

--- src/App.java
+++ src/App.java
@@ -1,20 +1,35 @@
-// TODO: Currently I have to manually add the id, this should be automated.
+/* TODO: Currently I have to manually add the id, this should be automated. */
+/* Way to go about it I think
+    What I could probably do is make a radar, that checks for a response to a question, like name, age, bla bla bla.
+    these get stored in a String, int etc.
+    Then I insert these using users.add.
+    I just need to check if I can give it variables.
+
+    This makes it interactive.
+
+    Still need to check for finding people. However I have the id, so I could probably print like a name.
+    Then with the corresponding id, for them to find it easily.
+
+    However they would have to remember id's. So I don't know why I added ID's
+
+    I could probably remove that, but for now its whatever and keep it, maybe its useful down the line.
+*/
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
-    record Person(int id, String firstName, String lastName) {}
+    record Person(int id, String firstName, String lastName, int age) {}
 
     List<Person> users = new ArrayList<>();
 
     void run() {
-        users.add(new Person(0, "Jane", "Doe"));
-        users.add(new Person(1, "John", "Doe"));
+        users.add(new Person(0, "Jane", "Doe", 42));
+        users.add(new Person(1, "John", "Doe",42));
 
         for (Person person : users) {
-            System.out.println((person.id() + ": " + person.firstName()));
+            System.out.println((person.id() + ": " + person.firstName() + " age: " + person.age()));
         }
     }
     
