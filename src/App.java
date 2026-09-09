6d780a8b6e990193e9aabfca540d828c3669ee08
ronansky <ronandrost20@gmail.com>
2026-09-09 21:31:01 +0200

    Implement Person record and update main method to create and print a user instance

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    record Person(int id, String firstName, String lastName) {}
+    
+    public static void main(String[] args){
+        Person user1 = new Person(1, "Jane", "Doe");
+
+        System.out.println(user1);
     }
 }
