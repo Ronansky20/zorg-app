381ab119dd99c4b01179685d86df4ef7b11cbc58
Ronansky <48455711+Ronansky20@users.noreply.github.com>
2026-09-10 12:09:59 +0200

    Implement patient data loading and saving functionality; enhance user experience with welcome message

--- src/App.java
+++ src/App.java
@@ -1,8 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
+    private static final Path DATA_FILE = Paths.get("patients.json");
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     Scanner scan = new Scanner(System.in);
 
@@ -36,6 +49,7 @@ public class App {
     }
 
     void run() {
+        load();
         System.out.println("Welcome to the patient management system!");
 
         boolean addingMore = true;
@@ -46,6 +60,30 @@ public class App {
         }
         for (Person person : users) {
             System.out.println((person.firstName() + " age: " + person.age() + " weight: " + person.weight() + " persons address: " + person.address()) + " Persons BMI: " + person.bmi());
+        }
+        save();
+    }
+
+    private void save() {
+        try (Writer writer = Files.newBufferedWriter(DATA_FILE)) {
+            gson.toJson(users, writer);
+        } catch (IOException e) {
+            System.out.println("Could not save: " + e.getMessage());
+        }
+    }
+
+    private void load() {
+        if (!Files.exists(DATA_FILE)) {
+            return;
+        }
+        try (Reader reader = Files.newBufferedReader(DATA_FILE)) {
+            List<Person> loaded = gson.fromJson(reader,
+                    new TypeToken<List<Person>>() {}.getType());
+            if (loaded != null) {
+                users.addAll(loaded);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not load: " + e.getMessage());
         }
     }
     
