c11e73003cc6dc8002165ef40ea86b2f49be1af1
Ronansky <48455711+Ronansky20@users.noreply.github.com>
2026-09-10 09:25:11 +0200

    Enhance Person record to include weight, length, bmi, and address; update user details printing

--- src/App.java
+++ src/App.java
@@ -20,16 +20,17 @@ import java.util.List;
 
 public class App {
 
-    record Person(int id, String firstName, String lastName, int age) {}
+    record Person(int id, String firstName, String lastName, int age, int weight, int length, double bmi, String address) {}
 
     List<Person> users = new ArrayList<>();
 
     void run() {
-        users.add(new Person(0, "Jane", "Doe", 42));
-        users.add(new Person(1, "John", "Doe",42));
+        users.add(new Person(0, "Jane", "Doe", 42, 50, 170, 22.5, "123 Main St"));
+        users.add(new Person(1, "John", "Doe",42, 42, 42, 22.5, "456 Elm St"));
+
 
         for (Person person : users) {
-            System.out.println((person.id() + ": " + person.firstName() + " age: " + person.age()));
+            System.out.println((person.id() + ": " + person.firstName() + " age: " + person.age() + " weight: " + person.weight()));
         }
     }
     
